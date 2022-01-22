e5a8c2aaf1d6b50a1d6b1fa0cee748adffafb0df
Gulnara Sibagatullina <googlya7@yandex.ru>
2022-01-22 16:38:57 +0500

    Added comment erorr

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-/*This is a...*/
+/*This program is just an example one under VCS*/
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
 int a, b;
-cout << "Enter two integer numbers"<<endl;
+cout << "Enter A and B"<<endl;
 cin>> a >>b;
-int c;
-c=a-b;
-cout << "a-b="<<c<<endl;
+cout << "A+B="<<a+b<<'\n'<<endl;
+cout << "A-B="<<a-b<<'\n'<<endl;
+cout << "A*B="<<a*b<<'\n'<<endl;
 }
